51fc3597be40307367d93dc17440a92a893c6966
Cristian Silva <cristian123105@gmail.com>
2020-12-02 20:08:48 -0300

    p2

--- p2/p2.sh
+++ p2/p2.sh
@@ -2,19 +2,39 @@
 clear;
 echo;
 
-read -p "Insira o nome de um usuário linux: " usuario;
+Encerrar() {
+    sleep 3;
+    exit;
+}
 
+read -p "Informe o Nome do Usuario Linux : " usuario
+echo;
 if [ -z $usuario ]; then
-    echo "Informe um parametro correto!";
-    sleep 3;
-    exit;
+    echo "Informe um Usuario Linux Correto";
+    Encerrar;
+fi 
+
+if [ -d $usuario ]; then # testa se é um diretório && se o arquivo existe && palavra não é nula
+   ls $usuario;
+else
+    echo "Esse Usuario Nao Existe!";
+    Encerrar;
 fi
 
-if [ "$(cat /etc/passwd | grep -i $usuario | cut -d ':' -f 1 | wc -l)" != "0" ]; then
-    cd /home/$usuario
-    ls -la | grep -i --color $usuario
-else
-    echo "Usuário não existe, informe um usuário valido"
-    sleep 3;
-    exit;
+cd $rota;
+
+if [ -e $arquivo ]; then # pra testar o arquivo vc tem que estar na pasta dele
+    if [ -s $arquivo ]; then
+        grep -n -i --color "$palavra" prova.txt
+    else
+        echo "O arquivo informado está vázio!";
+        EncerraPrograma;
+    fi
+else 
+    echo "O arquivo informa não existe!";
+    Encerrar;
 fi
+
+# if [ -n $palavra ]
+# else
+# fi
